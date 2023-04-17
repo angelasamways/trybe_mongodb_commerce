ff85f519f772cdcc40af371b0a8288ee8487cd7a
Angela Samways <angelasamways@gmail.com>
2023-04-17 16:36:32 -0300

    Req5 fix lint

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,16 +2,14 @@ db.produtos.find(
   {
     $or: [
       {
-        curtidas: { $eq: 36 }
+        curtidas: { $eq: 36 },
       },
       {
         vendidos: { $eq: 85 },
       },
-    ]
+    ],
   },
   {
-    nome: 1, curtidas: 1, vendidos: 1, _id: 0
+    nome: 1, curtidas: 1, vendidos: 1, _id: 0,
   },
 );
-
-// Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85
