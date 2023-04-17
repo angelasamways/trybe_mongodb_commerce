6689452894f5a248eea21ea8c24fc2349975a860
Angela Samways <angelasamways@gmail.com>
2023-04-17 16:43:54 -0300

    Req7

---
+++ challenges/desafio7.js
@@ -0,0 +1,18 @@
+db.produtos.find(
+  {
+    $and:
+    [
+      { 
+        vendidos: { $ne: 50 }
+      },
+      {
+        tags: { $exists: false }
+      }
+    ],
+  },
+  {
+    nome: 1, vendidos: 1, _id: 0,
+  },
+);
+
+// vendidos: { $ne: 50 },
